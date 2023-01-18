0784836f2a36f1d25143e134f2171881b52126f7
kidibra <kidibra2020@gmail.com>
2023-01-18 19:46:27 +0300

    fix: next page type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { NextPage } from "next";
 import { useState } from "react";
 
-const HomePage: NextPage = () => {
+const HomePage = () => {
 	const [data, setData] = useState({
 		name: "",
 		email: "",
